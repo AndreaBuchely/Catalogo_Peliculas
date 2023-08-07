c8f6bd8877cc2bf264f0ff8e90644adc4139472b
AndreaBuchely <abuchelymoreno@gmail.com>
2023-08-07 17:46:07 -0500

    Metodo str

---
+++ clases.py
@@ -0,0 +1,42 @@
+import os
+
+class Pelicula:
+    def __init__(self,nombre, año, calificacion):
+        self.__nombre = nombre
+        self.año = año
+        self.calificacion = calificacion
+    
+    def __str__(self):
+        return f"Nombre: {self.nombre} Año: {self.año} Calificación: {self.calificacion}"
+    
+    
+    #def __repr__(self):
+
+    # crear metodos de acceso: mostrar el atributo nombre y modificar
+    @property
+    def nombre(self):
+        return self.__nombre
+    
+    @nombre.setter
+    def nombre(self, nombre):
+        self.__nombre = nombre
+
+class CatalogoPelicula:
+    
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.ruta_archivo = f'{self.nombre}.txt'
+
+    def agregar_pelicula(self, pelicula):
+        with open(self.ruta_archivo, 'a') as archivo:
+            archivo.write(f'{pelicula.nombre}\n')
+
+    def listar_peliculas(self):
+        # abrir el archivo con with 
+        # imprimir lo que hay en el archivo ( arhivo.read() )
+        #con un print debo mostrar en pantalla todo el listado
+        pass
+
+    def eliminar_peliculas(self): #deberia ser eliminar catalogo
+        os.remove(self.ruta_archivo) #con esto se elimina el catalogo
+        print(f'Archivo eliminado: {self.ruta_archivo}')
---
+++ main.py
@@ -0,0 +1,31 @@
+from clases import *  # podia poner Pelicula, CatalogoPelicula, pero el * importa todo
+
+opcion = None
+
+nombre_catalogo = input("Ingrese nombre del catalogo:")
+catalogo = CatalogoPelicula(nombre_catalogo)
+
+while opcion != 4:
+    try:
+        print("Opciones: ")
+        print("1. Agregar Pelicula")
+        print("2. Listar Pelicula")
+        print("3. Eliminar catálogo películas")
+        print("4. Salir")
+
+        opcion = int(input("Escribe tu opción ( del 1 al 4):"))
+
+        if opcion == 1:
+            nombre_pelicula = input(" Indique el nombre de la pelicula:")
+            pelicula = Pelicula(nombre_pelicula)
+            catalogo.agregar_pelicula(pelicula)
+        elif opcion == 2:
+            #esta opc no va a funcionar hasta que se desarrolle el metodo listar peliculas de la clase CatalogoPelicula
+            catalogo.listar_peliculas()
+        elif opcion == 3:
+            catalogo.eliminar_peliculas()
+    except Exception as e:
+        print(f"Ocurrio un error: {e}")
+        opcion = 4
+else:
+    print("Programa finalizado")
